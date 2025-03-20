2b14f28b18a87305702dde4288107632d25ababf
LETICIA SOUZA ALMEIDA <24122661@dsn127m1246729.cfp127aluno.local>
2025-03-20 16:16:08 -0300

    Layout da página BMIResult concluído

---
+++ app/src/main/java/br/senai/sp/jandira/bmi/screens/BMIResultScreen.kt
@@ -0,0 +1,248 @@
+package br.senai.sp.jandira.bmi.screens
+
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Text
+import androidx.compose.material3.VerticalDivider
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.senai.sp.jandira.bmi.R
+
+@Composable
+fun BMIResultScreen(modifier: Modifier = Modifier) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize( )
+    ){
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    brush = Brush.horizontalGradient(
+                        listOf(
+                            Color(0xFF9B81DC),
+                            Color(0xFF6945A8)
+                        )
+                    )
+                )
+        ){
+            Text(
+                text = stringResource(
+                    R.string.your_bmi
+                ),
+                fontSize = 40.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.White,
+                modifier = Modifier
+                    .padding(20.dp)
+                    .padding(top = 30.dp)
+                    .weight(1f)
+            )
+            Card(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .weight(5f)
+                    .padding(top = 20.dp),
+                shape = RoundedCornerShape(
+                    topStart = 50.dp,
+                    topEnd = 50.dp
+                )
+            ){
+                Column(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(20.dp),
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ){
+                    Card(
+                        modifier = Modifier
+                            .size(150.dp),
+                        shape = CircleShape,
+                        border = BorderStroke(
+                            width = 7.dp,
+                            brush = Brush.horizontalGradient(
+                                listOf(
+                                    Color(0xFF451693),
+                                    Color(0xFF6945A8)
+                                )
+                            )
+                        )
+                    ){
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize(),
+                            verticalArrangement = Arrangement.Center,
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ){
+                            Text(
+                                text = "30,6",
+                                fontSize = 50.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color(0xFF451693)
+                            )
+                        }
+                    }
+                    Text(
+                        text = stringResource(
+                            R.string.result
+                        ),
+                        fontSize = 25.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black,
+                        modifier = Modifier
+                            .padding(15.dp)
+                    )
+                    Card(
+                        modifier = Modifier
+                            .width(370.dp)
+                            .height(100.dp),
+                        colors = CardDefaults
+                            .cardColors(
+                                containerColor = Color(0xABAFA8E1)
+                            )
+                    ){
+                        Row(
+                            modifier = Modifier
+                                .fillMaxWidth()
+                        ){
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxHeight()
+                                    .weight(1f),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ){
+                                Text(
+                                    text = stringResource(
+                                        R.string.age
+                                    ),
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    text = "17",
+                                    fontSize = 25.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color.Black
+                                )
+                            }
+                            VerticalDivider(
+                                modifier = Modifier
+                                    .padding(vertical = 10.dp),
+                                color = Color.Gray
+                            )
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxHeight()
+                                    .weight(1f),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ){
+                                Text(
+                                    text = stringResource(
+                                        R.string.weight
+                                    ),
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    text = "53 Kg",
+                                    fontSize = 25.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color.Black
+                                )
+                            }
+                            VerticalDivider(
+                                modifier = Modifier
+                                    .padding(vertical = 10.dp),
+                                color = Color.Gray
+                            )
+                            Column(
+                                modifier = Modifier
+                                    .fillMaxHeight()
+                                    .weight(1f),
+                                verticalArrangement = Arrangement.Center,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ){
+                                Text(
+                                    text = stringResource(
+                                        R.string.height
+                                    ),
+                                    fontSize = 20.sp,
+                                    fontWeight = FontWeight.Bold
+                                )
+                                Text(
+                                    text = "163 cm",
+                                    fontSize = 25.sp,
+                                    fontWeight = FontWeight.Bold,
+                                    color = Color.Black
+                                )
+                            }
+                        }
+                    }
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(200.dp)
+                            .padding(top = 10.dp)
+                            .background(color = Color.DarkGray)
+                    ){ }
+                    HorizontalDivider(
+                        modifier = Modifier
+                            .padding(top = 30.dp),
+                        color = Color.Gray
+                    )
+                    Button(
+                        onClick = {},
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(top = 30.dp),
+                        shape = RoundedCornerShape(10.dp),
+                        colors = ButtonDefaults.buttonColors(Color(0xFF451693))
+                    ){
+                        Text(
+                            text = stringResource(
+                                R.string.new_calc
+                            ),
+                            fontSize = 25.sp
+                        )
+                    }
+                }
+            }
+        }
+    }
+
+}
+
+@Preview (showSystemUi = true)
+@Composable
+private fun BMIResultScreenPreview() {
+    BMIResultScreen()
+}
